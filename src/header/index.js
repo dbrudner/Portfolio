1d112c40b61b5a06e4a4a1f48965bce499a3ae52
dbrudner <dbrudne1@gmail.com>
2018-03-12 22:00:17 -0400

    commit

--- src/header/index.js
+++ src/header/index.js
@@ -1,16 +1,12 @@
 import React from 'react'
 import Nav from '../nav'
 import styled from 'styled-components'
-import img from '../images/keyboard.jpg'
 
 export default function Header() {
 
     const Div = styled.div`
-        height: 50vh;
-        background-image: url(${img});
-        background-size: cover;
-        opacity: .4;
-        background-position: center;
+        height: 100vh;
+        background-color: #2098D1;
     `
 
     return (
--- src/nav/nav.js
+++ src/nav/nav.js
@@ -1,28 +1,79 @@
 import React from 'react'
 import styled from 'styled-components'
-
+import img from '../images/me_party_hat.jpg'
 
 export default function Nav() {
 
     const Ul = styled.ul`
         display: inline-block;
+        margin-top: 3.5rem;
     `
 
     const Nav = styled.nav`
-    
+        color: white;
+        font-family: 'Lato', sans-serif;
+        font-size: 2rem;
+        display: flex;
+        justify-content: space-between;
+        padding: 0 20%;
+        border-bottom: 3px solid #f3f3f3;
+        height: 10rem;
     `
 
     const Li = styled.li`
         display: inline;
+        margin-left: 4rem;
     `
 
+    const Brand = styled.div`
+        display: grid;
+        grid-template-columns: 10rem 20rem;
+        margin-top: 2.4rem;
+    `
+
+    const BrandHeader = styled.div`
+        h4 {
+            margin: 0
+            text-transform: uppercase;
+        }
+    `
+
+    const BrandDescription = styled.div`
+        p {
+            margin: 1rem 0 0 0;
+            font-size: 1.6rem;
+        }
+    `
+    const BrandImage = styled.div`
+        div {
+            margin-left: 3.5rem;
+        }
+    ` 
+    const iconStyle = {
+        fontSize: '5.5rem'
+    }
 
     return (
         <Nav>
+            <Brand>
+                <BrandImage>
+                    <div>
+                        <i style={iconStyle} className="fab fa-gg-circle"></i>
+                    </div>
+                </BrandImage>
+                <div>
+                    <BrandHeader>
+                        <h4>Dave Brudner</h4>
+                    </BrandHeader>
+                    <BrandDescription>
+                        <p>Full Stack Web Developer</p>
+                    </BrandDescription>
+                </div>
+            </Brand>
             <Ul>
-                <Li>Port</Li>
-                <Li>Flo</Li>
-                <Li>Yo</Li>
+                <Li>About</Li>
+                <Li>Portfolio</Li>
+                <Li>More</Li>
             </Ul>
         </Nav>
                 
